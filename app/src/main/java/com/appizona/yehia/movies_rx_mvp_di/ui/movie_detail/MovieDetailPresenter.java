d98a2ade73187d77cf45b6d9e659b14ae9214929
Mohamed Yehia <yehia_hd@yahoo.com>
2017-10-04 23:27:10 +0200

    Handle Presenter Lifecycle

--- app/src/main/java/com/appizona/yehia/movies_rx_mvp_di/ui/movie_detail/MovieDetailPresenter.java
+++ app/src/main/java/com/appizona/yehia/movies_rx_mvp_di/ui/movie_detail/MovieDetailPresenter.java
@@ -24,6 +24,7 @@ public class MovieDetailPresenter extends BasePresenter<MovieDetailView> {
 
     @Override
     public void detachView() {
+        compositeDisposable.clear();
         super.detachView();
     }
 
